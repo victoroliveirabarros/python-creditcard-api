9f96bf1adce30c4a4542c4a02fc52b7f3763de4f
Victor Oliveira Barros do Nascimento <victoroliveirabarros@outlook.com>
2023-09-26 15:25:32 -0300

    feat: get credit card usecase

---
+++ app/services/usecases/get_credit_card_usecase.py
@@ -0,0 +1,39 @@
+from http import HTTPStatus
+import datetime
+
+from app.domain.models import CreditCardModel
+from app.domain.usecases import Usecase
+
+from app.services.helpers.http.http import HttpResponse
+
+class GetCreditCardUsecase(Usecase):
+
+    def __init__(
+        self,
+        session,
+        id_card
+    ) -> None:
+        self.session = session
+        self.id_card = id_card
+
+    def execute(self, params: None) -> HttpResponse:
+        session = self.session()
+
+        # Retrieve the credit card with the specified ID from the database
+        credit_card = session.query(CreditCardModel).filter_by(id=self.id_card).first()
+
+        session.close()
+
+        if credit_card is not None:
+            credit_card_data = {
+                'id': credit_card.id,
+                'exp_date': datetime.datetime.strptime(credit_card.exp_date, '%Y-%m-%d').strftime('%Y-%m-%d'),
+                'holder': credit_card.holder,
+                'number': credit_card.number,
+                'cvv': credit_card.cvv,
+                'brand': credit_card.brand
+            }
+            return HttpResponse(HTTPStatus.OK, credit_card_data)
+
+        return HttpResponse(HTTPStatus.NOT_FOUND, 'Card Not Found')
+    
