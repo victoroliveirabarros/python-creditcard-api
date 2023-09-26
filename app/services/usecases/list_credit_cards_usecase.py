b32c4778952bfc7485f91c0c7023b38814fe242c
Victor Oliveira Barros do Nascimento <victoroliveirabarros@outlook.com>
2023-09-26 15:25:13 -0300

    feat: list credit cards

---
+++ app/services/usecases/list_credit_cards_usecase.py
@@ -0,0 +1,39 @@
+from http import HTTPStatus
+import datetime
+
+from app.domain.models import CreditCardModel
+from app.domain.usecases import  Usecase
+
+from app.services.helpers.http.http import HttpResponse
+
+class ListCreditCardsUsecase(Usecase):
+
+    def __init__(
+        self,
+        session
+    ) -> None:
+        self.session = session
+
+    def execute(self, params: None) -> HttpResponse:
+        session = self.session()
+
+        # Retrieve all credit cards from the database
+        credit_cards = session.query(CreditCardModel).all()
+
+        session.close()
+
+        # Convert credit card objects to dictionaries
+        credit_cards_data = [
+            {
+                'id': card.id,
+                'exp_date': datetime.datetime.strptime(card.exp_date, '%Y-%m-%d').strftime('%Y-%m-%d'),
+                'holder': card.holder,
+                'number': card.number,
+                'cvv': card.cvv,
+                'brand': card.brand
+            }
+            for card in credit_cards
+        ]
+
+        return HttpResponse(HTTPStatus.OK, credit_cards_data)
+    
